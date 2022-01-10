9754ab91c833819062e95620bf67ac060d133658
Charlie Henry <30810522+Charlie-Henry@users.noreply.github.com>
2022-01-10 13:03:32 -0600

    Adding documentation

--- meters/payments_s3.py
+++ meters/payments_s3.py
@@ -1,18 +1,16 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov 23 14:47:12 2021
+import ntpath
+import os
+import logging
+import argparse
+from datetime import datetime
 
-@author: charliehenry
-"""
 
 from pypgrest import Postgrest
 import pandas as pd
 import boto3
-import ntpath
-from sodapy import Socrata
 from dotenv import load_dotenv
-import os
+
+import utils
 
 # Envrioment variables
 load_dotenv("smartfolio.env")
@@ -21,24 +19,6 @@ AWS_ACCESS_ID = os.environ.get("AWS_ACCESS_ID")
 AWS_PASS = os.environ.get("AWS_PASS")
 BUCKET_NAME = os.environ.get("BUCKET_NAME")
 POSTGREST_TOKEN = os.environ.get("POSTGREST_TOKEN")
-
-arg_year = 2021
-arg_month = 12
-
-aws_s3_client = boto3.client(
-    "s3", aws_access_key_id=AWS_ACCESS_ID, aws_secret_access_key=AWS_PASS,
-)
-
-
-# Downloads a file from s3
-def download_s3_file(file_key):
-    """
-    Downloads an email file from s3
-    :param file_key: the full path to the email file
-    :return:
-    """
-    with open((get_file_name(file_key)), "wb") as f:
-        aws_s3_client.download_fileobj(BUCKET_NAME, file_key, f)
 
 
 def get_file_name(file_key):
@@ -50,13 +30,13 @@ def get_file_name(file_key):
     return ntpath.basename(file_key)
 
 
-def get_csv_list(year, month):
+def get_csv_list(year, month, client):
     """
     Returns an array of files parsed into an actual array (as opposed to an object)
     :return: array of strings
     """
     csv_file_list = []
-    pending_csv_list = aws_list_files(year, month)
+    pending_csv_list = aws_list_files(year, month, client)
     for csv_file in pending_csv_list:
         csv_file_list.append(csv_file)
 
@@ -64,13 +44,12 @@ def get_csv_list(year, month):
     return csv_file_list
 
 
-def aws_list_files(year, month):
+def aws_list_files(year, month, client):
     """
     Returns a list of email files.
     :return: object
     """
-    global aws_s3_client
-    response = aws_s3_client.list_objects(
+    response = client.list_objects(
         Bucket=BUCKET_NAME,
         Prefix="meters/dev/archipel_transactionspub/" + str(year) + "/" + str(month),
     )
@@ -79,114 +58,184 @@ def aws_list_files(year, month):
         yield content.get("Key")
 
 
-def banking_change(banking_id):
-    if banking_id == "0" or banking_id == "<NA>":
-        return "No Banking ID"
-    if len(banking_id) >= 6:
-        return banking_id
-    else:
-        output = "0" + banking_id
-        return banking_change(output)
+def get_invoice_id(banking_id, terminal_code):
+    """Create the Inovice ID which is a concatention of the banking ID and device ID
+
+    Args:
+        banking_id (int): whatever this is
+        terminal_code (int): whatever this is
+
+    Returns:
+        int: The formatted invoice ID
+    """
+    if pd.isna(banking_id) and terminal_code:
+        return -1
+
+    if banking_id == 0:
+        return -1
+
+    # get last 4 digits of terminal code
+    terminal_code = str(terminal_code)[-4:]
+
+    # zero-pad bank ID to 6 digits
+    banking_id = f"{banking_id:06d}"
+    invoice_id = f"{terminal_code}{banking_id}"
+    return int(invoice_id)
 
 
-def invoice_num(banking_id, terminal_code):
-    if banking_id == "No Banking ID":
-        return "-1"
-    out = terminal_code[-4:]
-    out = out + "" + banking_id
-    if out[0] == "0":
-        return out[-1 * len(out) + 1 :]
-    return out
+def postgres_datetime(time_field):
+    """Changes the existing datetime field in S3 to a format that can be stored by postgres.
+        First parses the string time as datetime type then outputs as string.
+    
+    Args:
+        time_field (string): Datetime field used by smartfolio. 
+        Sent in a lambda function from a pandas series.
+    
+    Returns:
+        output (string): Formatted datetime field that is compatable with postgres
+    """
+    output = pd.to_datetime(
+        time_field, format="%Y-%m-%d %H:%M:%S", infer_datetime_format=True
+    )
+    return str(output)
 
 
-# Get list of CSVs
-csv_file_list = get_csv_list(arg_year, arg_month)
+def transform(smartfolio):
+    """Formats and adds/drops columns of a dataframe from smartfolio to conform 
+        to postgres DB schema.
+    
+    Args:
+        smartfolio (pandas dataframe): The unformatted data stored in S3 from smartfolio.
+    
+    Returns:
+        smartfolio (pandas dataframe): Formatted dataframe that works with DB schema.
+    """
 
-# Go through all files and combine into a dataframe
-data = []
-for csv_f in csv_file_list:
-    # Parse the file
-    if ".csv" in csv_f:
+    ## Column wrangling
+    # Invoice ID for matching to fiserv creation
+    smartfolio["Banking Id"] = smartfolio["TRANSACTION_NUMBER"].astype("Int64")
+
+    smartfolio["Terminal Code"] = smartfolio["TERMINAL_ID"]
+
+    smartfolio["invoice_id"] = smartfolio.apply(
+        lambda x: get_invoice_id(x["Banking Id"], x["Terminal Code"]), axis=1
+    )
+
+    # Date formatting
+    smartfolio["transaction_date"] = smartfolio.apply(
+        lambda x: postgres_datetime(x["TRANSACTION_DATE"]), axis=1
+    )
+    smartfolio["processed_date"] = smartfolio.apply(
+        lambda x: postgres_datetime(x["TRANSACTION_HANDLING_DATE"]), axis=1
+    )
+
+    # All transactions are credit cards in this dataset
+    smartfolio["transaction_type"] = "Card"
+
+    # Renaming to match schema
+    smartfolio = smartfolio.rename(
+        columns={
+            "SCHEME": "card_type",
+            "TERMINAL_ID": "meter_id",
+            "TRANSACTION_AMOUNT": "amount",
+            "TRANSACTION_STATUS": "transaction_status",
+            "REMITTANCE_STATUS": "remittance_status",
+        }
+    )
+
+    # Data types to match schema
+    smartfolio["invoice_id"] = smartfolio["invoice_id"].astype(int)
+    smartfolio["meter_id"] = smartfolio["meter_id"].astype(int)
+
+    # Sometimes blank processed dates are present
+    smartfolio["processed_date"] = smartfolio["processed_date"].replace("NaT", None)
+
+    # Payload to DB
+    smartfolio = smartfolio[
+        [
+            "invoice_id",
+            "card_type",
+            "meter_id",
+            "transaction_type",
+            "transaction_date",
+            "transaction_status",
+            "remittance_status",
+            "processed_date",
+            "amount",
+        ]
+    ]
+
+    return smartfolio
+
+
+def to_postgres(smartfolio):
+    """Uploads the formatted dataframe to two different postgres DBs.
+        flowbird_transactions_raw - just for smartfolio aka flowbird data
+        transactions - a combined parking DB which will also include data from passport
+    
+    Args:
+        smartfolio (pandas dataframe): Formatted dataframe that works with DB schema.
+    
+    Returns:
+        None
+    """
+    # Upsert to database
+    payload = smartfolio.to_dict(orient="records")
+
+    client = Postgrest(
+        "http://127.0.0.1:3000",
+        token=POSTGREST_TOKEN,
+        headers={"Prefer": "return=representation"},
+    )
+
+    res = client.upsert(resource="flowbird_payments_raw", data=payload)
+
+
+def main(args):
+    # Arguments to pick which month to download from S3
+    year = args.year
+    month = args.month
+
+    # If args are missing, default to current month and/or year
+    if not year:
+        year = datetime.now().year
+
+    if not month:
+        month = datetime.now().month
+
+    aws_s3_client = boto3.client(
+        "s3", aws_access_key_id=AWS_ACCESS_ID, aws_secret_access_key=AWS_PASS,
+    )
+
+    # Get list of CSVs
+    csv_file_list = get_csv_list(year, month, aws_s3_client)
+
+    # Go through all files and combine into a dataframe
+    csv_file_list = [f for f in csv_file_list if f.endswith(".csv")]
+
+    for csv_f in csv_file_list:
+        # Parse the file
         response = aws_s3_client.get_object(Bucket=BUCKET_NAME, Key=csv_f)
         df = pd.read_csv(response.get("Body"))
-        data.append(df)
+        logger.debug(f"Loaded CSV File: {csv_f}")
 
-        print("Loaded CSV File: '%s'" % csv_f)
+        df = transform(df)
+        to_postgres(df)
 
 
-smartfolio = pd.concat(data, ignore_index=True)
+# CLI arguments definition
+parser = argparse.ArgumentParser()
 
-## Column wrangling
-# Invoice ID for matching to fiserv creation
-smartfolio["Banking Id"] = smartfolio["TRANSACTION_NUMBER"].astype("Int64").astype(str)
-
-smartfolio["Terminal Code"] = smartfolio["TERMINAL_ID"].astype(str)
-
-smartfolio["Banking Id"] = smartfolio["Banking Id"].apply(banking_change)
-
-smartfolio["invoice_id"] = smartfolio.apply(
-    lambda x: invoice_num(x["Banking Id"], x["Terminal Code"]), axis=1
+parser.add_argument(
+    "--year", type=int, help=f"Year of folder to select, defaults to current year",
 )
 
-# Date formatting
-smartfolio["transaction_date"] = pd.to_datetime(
-    smartfolio["TRANSACTION_DATE"],
-    format="%Y-%m-%d %H:%M:%S",
-    infer_datetime_format=True,
+parser.add_argument(
+    "--month", type=int, help=f"Month of folder to select. defaults to current month",
 )
 
-smartfolio["processed_date"] = pd.to_datetime(
-    smartfolio["TRANSACTION_HANDLING_DATE"],
-    format="%Y-%m-%d %H:%M:%S",
-    infer_datetime_format=True,
-)
+args = parser.parse_args()
 
-smartfolio["transaction_date"] = smartfolio["transaction_date"].astype(str)
-smartfolio["processed_date"] = smartfolio["processed_date"].astype(str)
+logger = utils.get_logger(__file__, level=logging.DEBUG)
 
-# All transactions are credit cards in this dataset
-smartfolio["transaction_type"] = "Card"
-
-# Renaming to match schema
-smartfolio = smartfolio.rename(
-    columns={
-        "SCHEME": "card_type",
-        "TERMINAL_ID": "meter_id",
-        "TRANSACTION_AMOUNT": "amount",
-        "TRANSACTION_STATUS": "transaction_status",
-        "REMITTANCE_STATUS": "remittance_status",
-    }
-)
-
-# Data types to match schema
-smartfolio["invoice_id"] = smartfolio["invoice_id"].astype(int)
-smartfolio["meter_id"] = smartfolio["meter_id"].astype(int)
-
-# Sometimes blank processed dates are present
-smartfolio["processed_date"] = smartfolio["processed_date"].replace("NaT", None)
-
-# Payload to DB
-smartfolio = smartfolio[
-    [
-        "invoice_id",
-        "card_type",
-        "meter_id",
-        "transaction_type",
-        "transaction_date",
-        "transaction_status",
-        "remittance_status",
-        "processed_date",
-        "amount",
-    ]
-]
-
-payload = smartfolio.to_dict(orient="records")
-
-# Upsert to postgres
-client = Postgrest(
-    "http://127.0.0.1:3000",
-    token=POSTGREST_TOKEN,
-    headers={"Prefer": "return=representation"},
-)
-
-two = client.upsert(resource="flowbird_payments_raw", data=payload)
+main(args)
